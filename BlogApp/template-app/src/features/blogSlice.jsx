ee24000784169395b48d81073e9192f94ee000c8
hakan <tasbeyhakan@gmail.com>
2024-03-06 23:50:31 +0300

    blogSlice added

---
+++ BlogApp/template-app/src/features/blogSlice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const blogSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = blogSlice.actions
+
+export default blogSlice.reducer
